3db452e0bf7cd002a3d7e603d0c213105a1ecf12
William Adams <git@nanotechstyles.com>
2015-03-26 06:46:50 -0700

    Added copyright notice to .cpp files Added Test/test_animwin project Added high precision #define constants Converted more macros to inline

--- graphicc.h
+++ graphicc.h
@@ -21,25 +21,43 @@ limitations under the License.
 
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
 
 typedef double REAL;
 
-// Some useful constants
-#ifndef M_PI
-#define M_PI			3.14159265358979323846264338327950288
-#define M_ROOT_PI		1.772453850905516027
-#define M_HALF_PI		1.57079632679489661923132169163975144
-#define M_QUARTER_PI	0.785398163397448309615660845819875721
-#define M_ONE_OVER_PI	0.318309886183790671537767526745028724
+// There are various sources for high precision numbers.  The well known
+// CRC books are one.  The ones used here come variously from the Graphics Gems
+// books, as well as the glm library, or direct derivation
+// the 'G_' prefix is used to avoid potential conflict with the 'M_' prefix that might
+// be used in math.h
+#define G_PI     3.141592653589793238462643383279502884197169399375105820975
+#define G_E      2.718281828459045235360287471352662497757247093699959574967
+#define G_SQRT2  1.414213562373095048801688724209698078569671875376948073177
+#define G_SQRT3  1.732050807568877293527446341505872366942805253810380628056
+#define G_GOLDEN 1.618033988749894848204586834365638117720309179805762862135
 
-#define M_E				2.71828182845904523536
-#define M_EULER			0.577215664901532860606
+// derived constants
+#define G_PITIMES2  (2.0*G_PI)
+#define G_PIOVER2   (0.5*G_PI)
+#define G_PIOVER4	(0.25*G_PI)
+#define G_ONE_OVER_PI	(1.0/G_PI)
+//#define G_ONE_OVER_PI	0.318309886183790671537767526745028724
+#define G_ROOT_PI		1.772453850905516027
 
-#endif	// M_PI
+#define G_DTOR      (G_PI/180.0)
+#define G_RTOD      (180.0/G_PI)
 
-#define M_GOLDEN_RATIO	1.61803398874989484820458683436563811
-#define DEGREES(radians) ((180 / M_PI) * radians)
-#define RADIANS(degrees) ((M_PI/180)*degrees)
+// macro and constant for base 2 logarithm
+#define G_LN2    0.693147180559945309417232121458176568075500134360255254121
+#define G_LOG2(val) (log(val)*(1.0/G_LN_2))
+
+#define G_EULER			0.577215664901532860606
+
+
+
+
+inline REAL DEGREES(const REAL radians) {return ((180 / G_PI) * radians);}
+inline REAL RADIANS(const REAL degrees) {return ((G_PI / 180)*degrees);}
 
 // map a value (a) from between rlo <= a <= rhi to  shi <= b <= slo
 inline double MAP(double a, double rlo, double rhi, double slo, double shi)
@@ -104,7 +122,8 @@ enum pixellayouts {
 
 // pixel buffer rectangle
 typedef struct _pb_rect {
-	unsigned int x, y, width, height;
+	int x, y;
+	int width, height;
 } pb_rect;
 
 inline int pb_rect_contains_point(const pb_rect &rct, const int x, const int y)
